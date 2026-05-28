e5beab5996ce96a76104bd290381faefdd84c90d
kolesie09 <asti020902@gmail.com>
2026-05-28 20:01:07 +0200

    Naprawiono dostęp do API z urządzeń w sieci lokalnej

--- Ignis-API/src/main/java/com/ignis/API/config/SecurityConfig.java
+++ Ignis-API/src/main/java/com/ignis/API/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.ignis.API.config;
 
+import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -21,6 +23,9 @@ public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
 
+    @Value("${app.cors.allowed-origin-patterns}")
+    private String allowedOriginPatterns;
+
     public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
         this.jwtAuthFilter = jwtAuthFilter;
     }
@@ -30,9 +35,20 @@ public class SecurityConfig {
         http
                 .cors(cors -> cors.configurationSource(request -> {
             var corsConfiguration = new CorsConfiguration();
-            corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173"));
-            corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+            corsConfiguration.setAllowedOriginPatterns(
+                    Arrays.stream(allowedOriginPatterns.split(","))
+                            .map(String::trim)
+                            .toList()
+            );
+
+            corsConfiguration.setAllowedMethods(List.of(
+                    "GET", "POST", "PUT", "DELETE", "OPTIONS"
+            ));
+
             corsConfiguration.setAllowedHeaders(List.of("*"));
+            corsConfiguration.setAllowCredentials(true);
+
             return corsConfiguration;
         }))
                 .csrf(csrf -> csrf.disable())
@@ -41,6 +57,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/users/login", "/api/users").permitAll()
+                .requestMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
                 .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
